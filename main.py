13b8aa06fc9552ed953ecae056d0f437a39b8c6d
Sebastian Schäffer <schaeffer@student.tugraz.at>
2021-11-08 13:47:22 +0100

    tic tac toe starting

---
+++ main.py
@@ -0,0 +1,170 @@
+import random
+import easygui
+import linecache
+import os
+from math import inf
+
+buffer = ["Ä", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+current_line_history = 0
+
+
+def display():
+    print("\n" * 100)
+    print(f"      |      "  "  |\n"
+          f" {buffer[7]}    |   {buffer[8]}  "f"  |  {buffer[9]}\n"
+          "      |      "  "  |\n"
+          "--------------------")
+    print(f"      |      "  "  |\n"
+          f" {buffer[4]}    |   {buffer[5]}  "f"  |  {buffer[6]}\n"
+          "      |      "  "  |\n"
+          "--------------------")
+    print(f"      |      "  "  |\n"
+          f" {buffer[1]}    |   {buffer[2]}  "f"  |  {buffer[3]}\n"
+          "      |      "  "  |\n")
+
+
+def user_choice(player):
+    choice = 'WRONG'
+    acceptable_range = range(1, 10)
+    within_range = False
+    while not within_range:
+        if current_line_history > 1:
+            print("You can use the command 'U' to undo a game step \n")
+        choice = input(f"Please Player {player} enter a number between 1 and 9:")
+        print(f"Currentlinehistory:{current_line_history}")
+        if not choice.isdigit():
+            if choice == "U":
+                if current_line_history == 1:
+                    print("Sorry, you cannot undo because there are no game steps")
+                    continue
+                undo()
+                return 0
+            print("Sorry, that is not a valid command!")
+            continue
+        elif int(choice) not in acceptable_range:
+            print("Sorry, you are out of acceptable range (1-9)")
+            continue
+        if not free_space_check(choice):
+            print("Sorry, this position is already taken!")
+            continue
+        within_range = True
+    return int(choice)
+
+
+def set_marker(marker, position):
+    if position == 0:
+        return
+    buffer[position] = marker
+    history_file()
+
+
+def win_check(mark, player_, computer=False):
+    win_pos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+    for x in win_pos:
+        check = True
+        for y in x:
+            if buffer[y] != mark:
+                check = False
+                break
+        if check:
+            if not computer:
+                display()
+                print(f"Player {player_} won the Game!")
+                undo()
+            return True
+    if " " not in buffer:
+        if not computer:
+            display()
+            print("DRAW!")
+        return True
+    return False
+
+
+def choose_first():
+    player_1 = random.randint(1, 2)
+    print(f"Player {player_1} starts the game!")
+    while True:
+        player_1_marker = input(f"Please Player {player_1} select X or O!")
+        if player_1_marker != "X" and player_1_marker != "O":
+            print("Sorry, that was not X nor O!")
+            continue
+        else:
+            break
+    player_2_marker = "X" if player_1_marker == "O" else "O"
+    player_2 = 2 if player_1 == 1 else 1
+    return {"player_1_marker": player_1_marker, "player_1": player_1,
+            "player_2_marker": player_2_marker, "player_2": player_2}
+
+
+def free_space_check(position):
+    return True if buffer[int(position)] == " " else False
+
+
+def replay():
+    while True:
+        x = input("Do you want to play again? (Yes/No)").lower()
+        if x != "yes" and x != "no":
+            continue
+        else:
+            break
+    if x == "yes":
+        for x in range(1, 10):
+            buffer[x] = " "
+        return True
+    return False
+
+
+def history_file():
+
+    global current_line_history
+    current_line_history += 1
+
+    with open('history.txt', "a") as file:
+        for field in buffer[1:]:
+            if field == " ":
+                file.write(" ")
+            elif field == "X":
+                file.write("X")
+            else:
+                file.write("O")
+        file.write("\n")
+
+
+def undo():
+
+    file_to_game = "history.txt"
+    global current_line_history
+    current_line_history -= 1
+    undo_history_line = linecache.getline(file_to_game, current_line_history)
+
+    for index, marker in enumerate(undo_history_line):
+        if len(buffer) != index + 1:
+            buffer[index + 1] = marker
+
+
+
+if __name__ == "__main__":
+    # file = easygui.fileopenbox()
+    # print(file) // load game
+    if os.path.exists("history.txt"):
+        os.remove("history.txt")
+
+    print("Welcome to Tic Tac Toe!")
+    history_file()
+    while True:
+        display()
+        player_dict = choose_first()
+        while True:
+            set_marker(player_dict["player_1_marker"], user_choice(player_dict["player_1"]))
+            display()
+
+            if win_check(player_dict["player_1_marker"], player_dict["player_1"]):
+                break
+
+            set_marker(player_dict["player_2_marker"], user_choice(player_dict["player_2"]))
+
+            display()
+            if win_check(player_dict["player_2_marker"], player_dict["player_2"]):
+                break
+        if not replay():
+            break
