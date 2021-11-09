b2a7f843a4376a30fdfcfe4ae731a206f2de2176
Hans-Jürgen Kleeberger <kleeberger@student.tugraz.at>
2021-11-09 23:20:56 +0100

    logic

--- main.py
+++ main.py
@@ -5,9 +5,36 @@ import os
 from tkinter import *
 from math import inf
 
-
 buffer = ["Ä", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 current_line_history = 0
+label_marker_list = list()
+
+
+def onEnter(event):
+    global player_count
+
+    if player_count == 1:
+        player_marker = "X"
+    else:
+        player_marker = "O"
+
+    choice = user_choice(player_count)
+    if choice == -1:
+        label10['text'] = "Invalid! [1-9] Pick [U] Undo [R] Redo"
+        return
+    set_marker(player_marker, choice)
+    display()
+    if win_check("X", player_count):
+        label10['text'] = "Player " + str(player_count) + " won the Game!"
+    else:
+        label10['text'] = "[1-9] Pick [U] Undo [R] Redo"
+
+    if player_count == 1:
+        player_count = 2
+    else:
+        player_count = 1
+
+    entry1.delete(0, END)
 
 
 def display():
@@ -24,34 +51,39 @@ def display():
           f" {buffer[1]}    |   {buffer[2]}  "f"  |  {buffer[3]}\n"
           "      |      "  "  |\n")
 
+    for index, field in enumerate(buffer):
+        if index == 0:
+            continue
+        print("Field: " + field + " Index: " + str(index))
+        if field == "X":
+            label_marker_list[index - 1]['image'] = cross_photo
+        if field == "O":
+            label_marker_list[index - 1]['image'] = circle_photo
+        if field == " ":
+            label_marker_list[index - 1]['image'] = blank_photo
+
 
 def user_choice(player):
     choice = 'WRONG'
     acceptable_range = range(1, 10)
     within_range = False
     while not within_range:
-        if current_line_history > 1:
-            print("You can use the command 'U' to undo a game step and 'R' to redo a game step \n")
-        choice = input(f"Please Player {player} enter a number between 1 and 9:")
+        choice = entry1.get()
 
         if not choice.isdigit():
             if choice == "U":
                 if current_line_history == 1:
-                    print("Sorry, you cannot undo because there are no game steps")
-                    continue
+                    return -1
                 undo(False)
                 return 0
             if choice == "R":
                 undo(True)
                 return 0
-            print("Sorry, that is not a valid command!")
-            continue
+            return -1
         elif int(choice) not in acceptable_range:
-            print("Sorry, you are out of acceptable range (1-9)")
-            continue
+            return -1
         if not free_space_check(choice):
-            print("Sorry, this position is already taken!")
-            continue
+            return -1
         within_range = True
     return int(choice)
 
@@ -171,11 +203,19 @@ def undo(redo):
 
 if __name__ == "__main__":
 
+    if os.path.exists("history.txt"):
+        os.remove("history.txt")
+    open('history.txt', "a")
+
+    player_1_marker = "X"
+    player_2_marker = "O"
+    player_count = 1
+
     tkwindow = Tk()
     tkwindow.title('TicTacToe')
     tkwindow.geometry("800x800")
     tkwindow.configure(bg="black")
-    tkwindow.resizable(0,0)
+    tkwindow.resizable(0, 0)
 
     tkwindow.columnconfigure(0, weight=3)
     tkwindow.columnconfigure(1, weight=3)
@@ -185,71 +225,82 @@ if __name__ == "__main__":
     tkwindow.rowconfigure(2, weight=3)
     cross_photo = PhotoImage(file='Cross3.png')
     circle_photo = PhotoImage(file='circle.png')
+    blank_photo = PhotoImage(file='blank.png')
 
     label1 = Label(master=tkwindow, bg='white', text="7", height=15, width=36, anchor=SE, font=('Helvetica', 9, 'bold'))
     label1.grid(row=0, column=0)
 
+    label1_marker = Label(master=tkwindow, image=blank_photo)
+    label1_marker.grid(row=0, column=0)
+
     label2 = Label(master=tkwindow, bg='white', text="4", height=16, width=36, anchor=SE, font=('Helvetica', 9, 'bold'))
     label2.grid(row=1, column=0)
 
-    label2_marker = Label(master=tkwindow, image=cross_photo)
+    label2_marker = Label(master=tkwindow, image=blank_photo)
     label2_marker.grid(row=1, column=0)
 
     label3 = Label(master=tkwindow, bg='white', text="1", height=16, width=36, anchor=SE, font=('Helvetica', 9, 'bold'))
     label3.grid(row=2, column=0)
 
-    label3_marker = Label(master=tkwindow, image=circle_photo)
+    label3_marker = Label(master=tkwindow, image=blank_photo)
     label3_marker.grid(row=2, column=0)
 
     label4 = Label(master=tkwindow, bg='white', text="8", height=15, width=36, anchor=SE, font=('Helvetica', 9, 'bold'))
     label4.grid(row=0, column=1)
 
+    label4_marker = Label(master=tkwindow, image=blank_photo)
+    label4_marker.grid(row=0, column=1)
+
     label5 = Label(master=tkwindow, bg='White', text="5", height=16, width=36, anchor=SE, font=('Helvetica', 9, 'bold'))
     label5.grid(row=1, column=1)
+
+    label5_marker = Label(master=tkwindow, image=blank_photo)
+    label5_marker.grid(row=1, column=1)
 
     label6 = Label(master=tkwindow, bg='White', text="2", height=16, width=36, anchor=SE, font=('Helvetica', 9, 'bold'))
     label6.grid(row=2, column=1)
 
+    label6_marker = Label(master=tkwindow, image=blank_photo)
+    label6_marker.grid(row=2, column=1)
+
     label7 = Label(master=tkwindow, bg='White', text="9", height=15, width=36, anchor=SE, font=('Helvetica', 9, 'bold'))
     label7.grid(row=0, column=2)
+
+    label7_marker = Label(master=tkwindow, image=blank_photo)
+    label7_marker.grid(row=0, column=2)
 
     label8 = Label(master=tkwindow, bg='White', text="6", height=16, width=36, anchor=SE, font=('Helvetica', 9, 'bold'))
     label8.grid(row=1, column=2)
 
+    label8_marker = Label(master=tkwindow, image=blank_photo)
+    label8_marker.grid(row=1, column=2)
+
     label9 = Label(master=tkwindow, bg='White', text="3", height=16, width=36, anchor=SE, font=('Helvetica', 9, 'bold'))
     label9.grid(row=2, column=2)
 
-    label10 = Label(master=tkwindow, bg='#FF0000', text="Welcome to TIC TAC TOE :)",  width=30, anchor= CENTER, font=('Helvetica', 9, 'bold'))
+    label9_marker = Label(master=tkwindow, image=blank_photo)
+    label9_marker.grid(row=2, column=2)
+
+    label10 = Label(master=tkwindow, bg='#FF0000', text="[1-9] Pick [U] Undo [R] Redo", width=30, anchor=CENTER,
+                    font=('Helvetica', 9, 'bold'))
     label10.grid(row=3, column=1)
 
     entry1 = Entry(master=tkwindow, bg='#FFFFFF', width="40")
     entry1.grid(row=4, column=1, padx='5', pady='5')
 
-    tkwindow.mainloop()
+    label_marker_list.append(label3_marker)
+    label_marker_list.append(label6_marker)
+    label_marker_list.append(label9_marker)
+    label_marker_list.append(label2_marker)
+    label_marker_list.append(label5_marker)
+    label_marker_list.append(label8_marker)
+    label_marker_list.append(label1_marker)
+    label_marker_list.append(label4_marker)
+    label_marker_list.append(label7_marker)
 
+    tkwindow.bind('<Return>', onEnter)
+
+    tkwindow.mainloop()
 
     # file = easygui.fileopenbox()
     # print(file) // load game
-    if os.path.exists("history.txt"):
-        os.remove("history.txt")
-    open('history.txt', "a")
-
-    print("Welcome to Tic Tac Toe!")
-    history_file()
-    while True:
-        display()
-        player_dict = choose_first()
-        while True:
-            set_marker(player_dict["player_1_marker"], user_choice(player_dict["player_1"]))
-            display()
-
-            if win_check(player_dict["player_1_marker"], player_dict["player_1"]):
-                break
-
-            set_marker(player_dict["player_2_marker"], user_choice(player_dict["player_2"]))
-
-            display()
-            if win_check(player_dict["player_2_marker"], player_dict["player_2"]):
-                break
-        if not replay():
-            break
