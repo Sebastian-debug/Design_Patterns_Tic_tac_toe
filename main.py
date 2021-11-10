114cc14920c9d273147c4b30f6ba1aa6fcece7ba
Sebastian Schäffer <schaeffer@student.tugraz.at>
2021-11-10 14:34:59 +0100

    logic done, new circle, cross

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-import random
+import shutil
+
 import easygui
 import linecache
 import os
 from tkinter import *
-from math import inf
+from shutil import copyfile
 
 buffer = ["Ä", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 current_line_history = 0
 label_marker_list = list()
+player_count = 1
 
 
-def onEnter(event):
+def onEnter(event=None):
     global player_count
 
     if player_count == 1:
@@ -18,21 +20,46 @@ def onEnter(event):
     else:
         player_marker = "O"
 
-    choice = user_choice(player_count)
+    choice = user_choice()
     if choice == -1:
-        label10['text'] = "Invalid! [1-9] Pick [U] Undo [R] Redo"
+        label10['text'] = "Invalid! [1-9] Pick [U] Undo [R] Redo [N] New\n  [L] Load [S] Save"
         return
+    if choice == -2:
+        global buffer
+        buffer = ["Ä", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+        player_count = 1
+        global current_line_history
+        current_line_history = 0
+        new_history_file()
+        display()
+        entry1.delete(0, END)
+        return
+    if choice == -3:
+        load_game()
+        display()
+        entry1.delete(0, END)
+        return
+    if choice == -4:
+        save_game()
+        display()
+        entry1.delete(0, END)
+        return
+
     set_marker(player_marker, choice)
     display()
-    if win_check(player_marker, player_count):
-        label10['text'] = "Player " + str(player_count) + " won the Game!"
+    if win_check(player_marker):
+        pass
     else:
-        label10['text'] = "[1-9] Pick [U] Undo [R] Redo"
+        label10['text'] = "[1-9] Pick [U] Undo [R] Redo [N] New\n  [L] Load [S] Save"
 
     if player_count == 1:
         player_count = 2
+        label11["text"] = "Player O"
+        label11["bg"] = "pink"
     else:
         player_count = 1
+        label11["text"] = "Player X"
+        label11["bg"] = "green"
 
     entry1.delete(0, END)
 
@@ -51,25 +78,23 @@ def display():
           f" {buffer[1]}    |   {buffer[2]}  "f"  |  {buffer[3]}\n"
           "      |      "  "  |\n")
 
-    for index, field in enumerate(buffer):
-        if index == 0:
-            continue
+    print(f"current_line_history: {current_line_history}\n")
+    for index, field in enumerate(buffer[1:]):
         print("Field: " + field + " Index: " + str(index))
         if field == "X":
-            label_marker_list[index - 1]['image'] = cross_photo
+            label_marker_list[index]['image'] = cross_photo
         if field == "O":
-            label_marker_list[index - 1]['image'] = circle_photo
+            label_marker_list[index]['image'] = circle_photo
         if field == " ":
-            label_marker_list[index - 1]['image'] = blank_photo
+            label_marker_list[index]['image'] = blank_photo
 
 
-def user_choice(player):
+def user_choice():
     choice = 'WRONG'
     acceptable_range = range(1, 10)
     within_range = False
     while not within_range:
         choice = entry1.get()
-
         if not choice.isdigit():
             if choice == "U":
                 if current_line_history == 1:
@@ -79,6 +104,12 @@ def user_choice(player):
             if choice == "R":
                 undo(True)
                 return 0
+            if choice == "N":
+                return -2
+            if choice == "L":
+                return -3
+            if choice == "S":
+                return -4
             return -1
         elif int(choice) not in acceptable_range:
             return -1
@@ -95,7 +126,7 @@ def set_marker(marker, position):
     history_file()
 
 
-def win_check(mark, player_, computer=False):
+def win_check(mark):
     win_pos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
     for x in win_pos:
         check = True
@@ -104,32 +135,14 @@ def win_check(mark, player_, computer=False):
                 check = False
                 break
         if check:
-            if not computer:
-                display()
-                print(f"Player {player_} won the Game!")
+            label10['text'] = "Player " + str(player_count) + " won the Game!"
+            display()
             return True
     if " " not in buffer:
-        if not computer:
-            display()
-            print("DRAW!")
+        label10['text'] = "DRAW!"
+        display()
         return True
     return False
-
-
-def choose_first():
-    player_1 = random.randint(1, 2)
-    print(f"Player {player_1} starts the game!")
-    while True:
-        player_1_marker = input(f"Please Player {player_1} select X or O!")
-        if player_1_marker != "X" and player_1_marker != "O":
-            print("Sorry, that was not X nor O!")
-            continue
-        else:
-            break
-    player_2_marker = "X" if player_1_marker == "O" else "O"
-    player_2 = 2 if player_1 == 1 else 1
-    return {"player_1_marker": player_1_marker, "player_1": player_1,
-            "player_2_marker": player_2_marker, "player_2": player_2}
 
 
 def free_space_check(position):
@@ -140,27 +153,11 @@ def get_file_line_numbers(file):
     return sum(1 for _ in open(file))
 
 
-def replay():
-    while True:
-        x = input("Do you want to play again? (Yes/No)").lower()
-        if x != "yes" and x != "no":
-            continue
-        else:
-            break
-    if x == "yes":
-        for x in range(1, 10):
-            buffer[x] = " "
-        if os.path.exists("history.txt"):
-            os.remove("history.txt")
-        return True
-    return False
-
-
 def history_file():
     global current_line_history
 
-    with open('history.txt', "a+") as file:
-        if current_line_history != get_file_line_numbers("history.txt"):
+    with open(file_name, "a+") as file:
+        if current_line_history != get_file_line_numbers(file_name):
             file.seek(0)
             tmp_history_lines = file.readlines()
             file.seek(0)
@@ -181,12 +178,12 @@ def history_file():
 
 
 def undo(redo):
-    file_to_game = "history.txt"
+    file_to_game = file_name
     global current_line_history
 
     if redo:
         current_line_history += 1
-        if get_file_line_numbers("history.txt") < current_line_history:
+        if get_file_line_numbers(file_name) < current_line_history:
             current_line_history -= 1
             print("Cannot redo, there are no steps ahead!")
             return
@@ -201,15 +198,54 @@ def undo(redo):
             buffer[index + 1] = marker
 
 
-if __name__ == "__main__":
+def new_history_file():
+    if os.path.exists(file_name):
+        os.remove(file_name)
+    open(file_name, "a")
 
-    if os.path.exists("history.txt"):
-        os.remove("history.txt")
-    open('history.txt', "a")
+
+def load_game():
+    file_name_loaded_game = easygui.fileopenbox()
+    global file_name
+    file_name = file_name_loaded_game
+    open(file_name, "a")
+
+    linecache.clearcache()
+    last_line_new_file = linecache.getline(file_name, get_file_line_numbers(file_name))
+
+    for line_index in range(len(last_line_new_file)):
+        if line_index+1 < len(buffer) and last_line_new_file[line_index] != "\n":
+            buffer[line_index+1] = last_line_new_file[line_index]
+
+    global current_line_history
+
+    current_line_history = get_file_line_numbers(file_name)
+    global player_count
+    player_count = 1 if current_line_history % 2 else 0
+
+
+def save_game():
+
+    saved_file_name = easygui.filesavebox(filetypes=['*.txt'])
+
+    if saved_file_name[-4:] != ".txt":
+        saved_file_name += ".txt"
+
+    try:
+        copyfile(file_name, saved_file_name)
+    except shutil.SameFileError:
+        pass
+
+
+
+
+if __name__ == "__main__":
+    file_name = "history.txt"
+    new_history_file()
+    history_file()
 
     player_1_marker = "X"
     player_2_marker = "O"
-    player_count = 1
 
     tkwindow = Tk()
     tkwindow.title('TicTacToe')
@@ -281,12 +317,17 @@ if __name__ == "__main__":
     label9_marker = Label(master=tkwindow, image=blank_photo)
     label9_marker.grid(row=2, column=2)
 
-    label10 = Label(master=tkwindow, bg='#FF0000', text="[1-9] Pick [U] Undo [R] Redo", width=30, anchor=CENTER,
+    label10 = Label(master=tkwindow, bg='#FF0000', text="[1-9] Pick [U] Undo [R] Redo [N] New\n  [L] Load [S] Save", width=30,
+                    anchor=CENTER,
+                    font=('Helvetica', 8, 'bold'))
+    label10.grid(row=4, column=1)
+
+    label11 = Label(master=tkwindow, bg='green', text="Player X", width=20, anchor=CENTER,
                     font=('Helvetica', 9, 'bold'))
-    label10.grid(row=3, column=1)
+    label11.grid(row=4, column=2)
 
     entry1 = Entry(master=tkwindow, bg='#FFFFFF', width="40")
-    entry1.grid(row=4, column=1, padx='5', pady='5')
+    entry1.grid(row=5, column=1, padx='5', pady='5')
 
     label_marker_list.append(label3_marker)
     label_marker_list.append(label6_marker)
@@ -301,6 +342,3 @@ if __name__ == "__main__":
     tkwindow.bind('<Return>', onEnter)
 
     tkwindow.mainloop()
-
-    # file = easygui.fileopenbox()
-    # print(file) // load game
