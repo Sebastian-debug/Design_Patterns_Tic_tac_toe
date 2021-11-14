cc500784513a30ec2045b0ec255ea579fc282b42
Sebastian <schaeffer@student.tugraz.at>
2021-11-14 18:14:20 +0100

    view optimized, refactored

--- controller.py
+++ controller.py
@@ -18,9 +18,9 @@ class Controller(object):
         else:
             player_marker = "O"
 
-        choice = self.model.user_choice(self.view.entry1.get())
+        choice = self.model.user_choice(self.view.user_entry.get())
         if choice == -1:
-            self.view.label10['text'] = "Invalid! [1-9] Pick [U] Undo [R] Redo [N] New\n  [L] Load [S] Save"
+            self.view.label_start['text'] = "Invalid! [1-9] Pick [U] Undo [R] Redo [N] New\n  [L] Load [S] Save"
             return
         if choice == -2:
             self.model.buffer = ["Ä", " ", " ", " ", " ", " ", " ", " ", " ", " "]
@@ -28,37 +28,37 @@ class Controller(object):
             self.model.current_line_history = 0
             self.model.createFile()
             self.view.display(self.model.buffer, self.model.current_line_history)
-            self.view.entry1.delete(0, END)
+            self.view.user_entry.delete(0, END)
             return
         if choice == -3:
             self.player_count = 1 if self.model.load_game() else 2
             self.view.display(self.model.buffer, self.model.current_line_history)
-            self.view.entry1.delete(0, END)
+            self.view.user_entry.delete(0, END)
             return
         if choice == -4:
             self.model.save_game()
             self.view.display(self.model.buffer, self.model.current_line_history)
-            self.view.entry1.delete(0, END)
+            self.view.user_entry.delete(0, END)
             return
 
         self.model.set_marker(player_marker, choice)
         self.view.display(self.model.buffer, self.model.current_line_history)
         win_check_ret = self.model.win_check(player_marker, self.player_count)
         if win_check_ret:
-            self.view.label10["text"] = win_check_ret
+            self.view.label_start["text"] = win_check_ret
             self.view.display(self.model.buffer, self.model.current_line_history)
             pass
         else:
             self.view.display(self.model.buffer, self.model.current_line_history)
-            self.view.label10['text'] = "[1-9] Pick [U] Undo [R] Redo [N] New\n  [L] Load [S] Save"
+            self.view.label_start['text'] = "[1-9] Pick [U] Undo [R] Redo [N] New\n  [L] Load [S] Save"
 
         if self.player_count == 1:
             self.player_count = 2
-            self.view.label11["text"] = "Player O"
-            self.view.label11["bg"] = "pink"
+            self.view.label_players["text"] = "Player O"
+            self.view.label_players["bg"] = "pink"
         else:
             self.player_count = 1
-            self.view.label11["text"] = "Player X"
-            self.view.label11["bg"] = "green"
+            self.view.label_players["text"] = "Player X"
+            self.view.label_players["bg"] = "green"
 
-        self.view.entry1.delete(0, END)
+        self.view.user_entry.delete(0, END)
