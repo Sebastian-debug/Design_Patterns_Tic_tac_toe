262eba7fd27b660ce033872c4ee5dd2eccfe6516
Hans-Jürgen Kleeberger <kleeberger@student.tugraz.at>
2021-11-16 10:55:48 +0100

    strategy

--- main.py
+++ main.py
@@ -2,7 +2,26 @@ from controller import *
 from PlayerVsPlayer import *
 from PlayerVsComputer import *
 from user_strategy import *
+from tkinter import *
+
+root = Tk()
+
+strategy = ""
+
+
+def chooseStrategy(playstyle):
+    global strategy
+    if playstyle:
+        strategy = PlayerVsPlayer()
+    else:
+        strategy = PlayerVsComputer()
+    root.destroy()
+
 
 if __name__ == "__main__":
-    strategy = PlayerVsPlayer()
+    Label(root, text="Choose your playstyle:\n", ).grid(row=0)
+    Button(root, text="Player vs Player", command=lambda: chooseStrategy(True)).grid(column=0, row=1)
+    Button(root, text="Player vs Computer", command=lambda: chooseStrategy(False)).grid(column=1, row=1)
+    mainloop()
+
     c = Controller(strategy)
