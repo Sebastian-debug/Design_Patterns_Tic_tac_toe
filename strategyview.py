57125fc907c53a5ead334f8e47567c5fc0f11c42
Sebastian <schaeffer@student.tugraz.at>
2021-11-16 13:15:30 +0100

    strategyview gui implemented

--- strategyview.py
+++ strategyview.py
@@ -9,10 +9,26 @@ from PlayerVsComputer import *
 class StrategyView(Tk):
     def __init__(self):
         Tk.__init__(self)
-        self.label = Label(self, text="Choose your playstyle:\n", ).grid(row=0)
-        self.button_playervsplayer = Button(self, text="Player vs Player",
-                                            command=lambda: self.chooseStrategy(True)).grid(column=0, row=1)
-        Button(self, text="Player vs Computer", command=lambda: self.chooseStrategy(False)).grid(column=1, row=1)
+        self.title("Game Mode")
+        self.geometry("410x410")
+        self.configure(bg="black")
+        self.resizable(0, 0)
+        self.columnconfigure(0, weight=3)
+        self.rowconfigure(1, weight=1)
+        self.label_playstyle = Label(master=self, bg='gray', text=f"Choose your playstyle:", height=5, width=35, anchor=CENTER,
+                           font=('Helvetica', 14, 'bold'))
+        self.label_playstyle.grid(column=0, row=0)
+        self.label_playstyle.place(x=0, y=0)
+        button_player = Button(self, text="Player vs Player", command=lambda: self.chooseStrategy(True), bg='green',
+                               width=16, height=13,
+                               anchor=CENTER, font=('Helvetica', 16, 'bold'))
+        button_player.grid(column=0, row=1)
+        button_player.place(x=0, y=100)
+        button_computer = Button(self, text="Player vs Computer", command=lambda: self.chooseStrategy(False), bg='red',
+                                 width=16, height=13,
+                                 anchor=CENTER, font=('Helvetica', 16, 'bold'))
+        button_computer.grid(column=1, row=1)
+        button_computer.place(x=200, y=100)
 
     def chooseStrategy(self, playstyle):
         if playstyle:
